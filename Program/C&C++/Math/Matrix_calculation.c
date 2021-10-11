b6b69729652ca0b7f8707c32ca03b53c703f2c41
DoudiNCer <HAPPYBOY5s2D3w5@outlook.com>
2021-10-11 17:44:49 +0800

    科协培训笔记

---
+++ Program/C&C++/Math/Matrix_calculation.c
@@ -0,0 +1,26 @@
+//
+#include <stdio.h>
+int main(void){
+	int model
+	int r1,c1,r2,c2;
+	do{
+
+		if (model == 0) break;
+	}while (0);
+	return 0;
+}
+
+//
+int multiplication(int const *matrix1,int const *matrix2){
+	return ret;
+}
+
+//
+int adjoint(int const *matrix,){
+	return ret;
+}
+
+//
+int inverse(int const *matrix){
+	return ret;
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # IWnA
 Some of my interesting and useful programming works and articles.
+
+## Programming Works  
+
+## Articles
+
++ [Training Essay](Articles\TrainingEssay.md)科协培训笔记
+
+## System or software config files
+
